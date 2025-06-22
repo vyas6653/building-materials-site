0ca7571af272585ec3425de1200abfe2a252da50
abvyas <vyas4396@gmail.com>
2025-06-22 14:42:50 +0530

    Backend code for Render deployment

--- building-materials-backend/db.js
+++ building-materials-backend/db.js
@@ -3,12 +3,7 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+const db = mysql.createConnection(process.env.DATABASE_URL);
 
 db.connect(err => {
   if (err) throw err;

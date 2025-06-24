4779e30de3cb1943527af49cab00c68e19b028fb
abvyas <vyas4396@gmail.com>
2025-06-24 15:12:21 +0530

    Backend for building materials

--- building-materials-backend/db.js
+++ building-materials-backend/db.js
@@ -1,13 +1,21 @@
 const mysql = require('mysql2');
 const dotenv = require('dotenv');
-
 dotenv.config();
 
-const db = mysql.createConnection(process.env.DATABASE_URL);
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
+});
 
 db.connect(err => {
-  if (err) throw err;
-  console.log('MySQL Connected');
+  if (err) {
+    console.error('Error connecting to MySQL:', err);
+    return;
+  }
+  console.log('✅ MySQL Connected');
 });
 
 module.exports = db;

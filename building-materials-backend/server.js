7f9c0c506ff6a52746cf89c6a51a3b00e8617f51
abvyas <vyas4396@gmail.com>
2025-06-28 13:06:54 +0530

    update image URL to use BACKEND_URL env variable

--- building-materials-backend/server.js
+++ building-materials-backend/server.js
@@ -29,5 +29,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 app.get('/', (req, res) => {
-  res.send("Backend is running 🎉");
+  res.send('API is running 🚀');
 });
+

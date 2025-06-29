c0c32dfc48895170fee9bbb61cf72d21a7efcc77
abvyas <vyas4396@gmail.com>
2025-06-29 13:42:26 +0530

    Fix: renamed API to axiosInstance to resolve redeclaration error

--- building-materials-frontend/my-react-app/src/api/api.js
+++ building-materials-frontend/my-react-app/src/api/api.js
@@ -1,19 +1,14 @@
 import axios from 'axios';
 import { getToken } from '../utils/auth';
 
-const API = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL + '/api', // Use environment variable
+const axiosInstance = axios.create({
+  baseURL: process.env.REACT_APP_API_BASE_URL + '/api',
 });
 
-const API = axios.create({
-  baseURL: 'https://building-materials-site-dlwn.onrender.com/api/auth',
-});
-
-// Add token to headers
-API.interceptors.request.use((config) => {
-  const token = getToken(); 
-    if (token) config.headers.Authorization = `Bearer ${token}`;
+axiosInstance.interceptors.request.use((config) => {
+  const token = getToken();
+  if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
 
-export default API;
+export default axiosInstance;
